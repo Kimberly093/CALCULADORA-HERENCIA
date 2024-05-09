79bf75b6a2edf7e13226f436dc756f2b586a1f3a
Kimberly <164302586+Kimberly093@users.noreply.github.com>
2024-05-09 02:04:37 -0600

    Add files via upload

---
+++ HERENCIA1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace HERENCIA1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sumar = new System.Windows.Forms.Button();
+            this.restar = new System.Windows.Forms.Button();
+            this.multiplicar = new System.Windows.Forms.Button();
+            this.dividir = new System.Windows.Forms.Button();
+            this.limpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.valor1 = new System.Windows.Forms.TextBox();
+            this.valor2 = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sumar
+            // 
+            this.sumar.BackColor = System.Drawing.Color.SkyBlue;
+            this.sumar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumar.Location = new System.Drawing.Point(567, 34);
+            this.sumar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sumar.Name = "sumar";
+            this.sumar.Size = new System.Drawing.Size(210, 75);
+            this.sumar.TabIndex = 0;
+            this.sumar.Text = "Sumar";
+            this.sumar.UseVisualStyleBackColor = false;
+            this.sumar.Click += new System.EventHandler(this.sumar_Click);
+            // 
+            // restar
+            // 
+            this.restar.BackColor = System.Drawing.Color.SkyBlue;
+            this.restar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.restar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restar.Location = new System.Drawing.Point(567, 118);
+            this.restar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.restar.Name = "restar";
+            this.restar.Size = new System.Drawing.Size(210, 75);
+            this.restar.TabIndex = 1;
+            this.restar.Text = "Restar";
+            this.restar.UseVisualStyleBackColor = false;
+            this.restar.Click += new System.EventHandler(this.restar_Click);
+            // 
+            // multiplicar
+            // 
+            this.multiplicar.BackColor = System.Drawing.Color.SkyBlue;
+            this.multiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.multiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicar.Location = new System.Drawing.Point(567, 203);
+            this.multiplicar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.multiplicar.Name = "multiplicar";
+            this.multiplicar.Size = new System.Drawing.Size(210, 75);
+            this.multiplicar.TabIndex = 2;
+            this.multiplicar.Text = "Multiplicar";
+            this.multiplicar.UseVisualStyleBackColor = false;
+            this.multiplicar.Click += new System.EventHandler(this.multiplicar_Click);
+            // 
+            // dividir
+            // 
+            this.dividir.BackColor = System.Drawing.Color.SkyBlue;
+            this.dividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dividir.Location = new System.Drawing.Point(567, 288);
+            this.dividir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dividir.Name = "dividir";
+            this.dividir.Size = new System.Drawing.Size(210, 75);
+            this.dividir.TabIndex = 3;
+            this.dividir.Text = "Dividir";
+            this.dividir.UseVisualStyleBackColor = false;
+            this.dividir.Click += new System.EventHandler(this.dividir_Click);
+            // 
+            // limpiar
+            // 
+            this.limpiar.BackColor = System.Drawing.Color.SkyBlue;
+            this.limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpiar.Location = new System.Drawing.Point(567, 372);
+            this.limpiar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.limpiar.Name = "limpiar";
+            this.limpiar.Size = new System.Drawing.Size(210, 75);
+            this.limpiar.TabIndex = 4;
+            this.limpiar.Text = "Limpiar";
+            this.limpiar.UseVisualStyleBackColor = false;
+            this.limpiar.Click += new System.EventHandler(this.limpiar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Valor 1";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 222);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 339);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado";
+            // 
+            // valor1
+            // 
+            this.valor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valor1.Location = new System.Drawing.Point(242, 94);
+            this.valor1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.valor1.Name = "valor1";
+            this.valor1.Size = new System.Drawing.Size(250, 43);
+            this.valor1.TabIndex = 8;
+            this.valor1.TextChanged += new System.EventHandler(this.valor1_TextChanged);
+            // 
+            // valor2
+            // 
+            this.valor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valor2.Location = new System.Drawing.Point(242, 219);
+            this.valor2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.valor2.Name = "valor2";
+            this.valor2.Size = new System.Drawing.Size(250, 43);
+            this.valor2.TabIndex = 9;
+            // 
+            // resultado
+            // 
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(242, 339);
+            this.resultado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(250, 43);
+            this.resultado.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(955, 637);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.valor2);
+            this.Controls.Add(this.valor1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.limpiar);
+            this.Controls.Add(this.dividir);
+            this.Controls.Add(this.multiplicar);
+            this.Controls.Add(this.restar);
+            this.Controls.Add(this.sumar);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sumar;
+        private System.Windows.Forms.Button restar;
+        private System.Windows.Forms.Button multiplicar;
+        private System.Windows.Forms.Button dividir;
+        private System.Windows.Forms.Button limpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox valor1;
+        private System.Windows.Forms.TextBox valor2;
+        private System.Windows.Forms.TextBox resultado;
+    }
+}
+
